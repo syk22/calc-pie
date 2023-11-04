48cf0e2b16c6e1f2889fa642364c6ce2a0edff01
SayakaNakajima <andsayaka@gmail.com>
2023-11-04 19:09:40 +0900

    とりあえずタイマーを設定

--- src/components/parts/Timer.tsx
+++ src/components/parts/Timer.tsx
@@ -3,10 +3,15 @@ import { useInterval } from '../../hooks/useInterval';
 
 export const Timer = () => {
   const [timer, setTimer] = useState<number>(0);
-  useInterval({
+  const { timerControl } = useInterval({
     onUpdate: () => {
       setTimer((t) => t + 1);
     },
   });
-  return <p>Time: {timer / 100}</p>;
+  return (
+    <>
+      <p>Time: {(timer / 100).toFixed(2)}</p>
+      <button onClick={() => timerControl()}>stop / start</button>
+    </>
+  );
 };
--- src/hooks/useInterval.tsx
+++ src/hooks/useInterval.tsx
@@ -1,11 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { FnType } from '../types/multipleTypes';
+
 interface Props {
-  onUpdate: () => void;
+  onUpdate: FnType;
 }
 
-export const useInterval = ({ onUpdate }: Props) => {
+interface Control {
+  timerControl: FnType;
+}
+
+export const useInterval = ({ onUpdate }: Props): Control => {
+  const onUpdateRef = useRef<FnType>();
+
+  const [isRunning, setIsRunning] = useState<boolean>(true);
+
+  const timerControl: FnType = () => setIsRunning((bool) => !bool);
   useEffect(() => {
-    const timerId = setInterval(() => onUpdate(), 10);
-    return () => clearInterval(timerId);
-  });
+    onUpdateRef.current = onUpdate;
+  }, []);
+  useEffect(() => {
+    let timerId: NodeJS.Timeout | undefined;
+
+    if (isRunning) {
+      timerId = setInterval(() => onUpdateRef.current?.(), 10);
+    } else {
+      timerId && clearInterval(timerId);
+    }
+    return () => timerId && clearInterval(timerId);
+  }, [isRunning]);
+  return { timerControl };
 };
