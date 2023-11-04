d991e978f4e942a017610a6377a87ef98f62a508
SayakaNakajima <andsayaka@gmail.com>
2023-11-05 01:04:19 +0900

    配列をやめて乱数発生のみに変更、エクスポートさせる

--- src/logic/multipleLogic.ts
+++ src/logic/multipleLogic.ts
@@ -1,13 +1,19 @@
-const generateRandomNum = (max: number) => {
-  return Math.ceil(Math.random() * max);
+export const generateRandomNum = (max: number) => {
+  const randomNum = () => {
+    return Math.ceil(Math.random() * max);
+  };
+  let term = randomNum();
+  if (term === 0) while (term === 0) term = randomNum();
+  console.log(term);
+  return term;
 };
 
-export const getNewArray = (amount: number, max: number): number[] => {
-  const returnArray: number[] = [];
-  for (let _ = 0; _ < amount; _++) {
-    let a = 0;
-    while (a === 0) a = generateRandomNum(max);
-    returnArray.push(a);
-  }
-  return returnArray;
-};
+// export const getNewArray = (amount: number, max: number): number[] => {
+//   const returnArray: number[] = [];
+//   for (let _ = 0; _ < amount; _++) {
+//     let a = 0;
+//     while (a === 0) a = generateRandomNum(max);
+//     returnArray.push(a);
+//   }
+//   return returnArray;
+// };
