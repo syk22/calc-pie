4c79c7ee0ed7dca86eab535a48e0605c67eb9520
SayakaNakajima <andsayaka@gmail.com>
2023-11-05 01:05:05 +0900

    型情報修正

--- src/types/multipleTypes.ts
+++ src/types/multipleTypes.ts
@@ -1,7 +1,11 @@
-export interface Record {
+export interface ResultRecordType {
   no: number;
   termA: number;
   termB: number;
   mistakes: number;
   time: number;
 }
+
+export type PageModeType = 'calc' | 'result';
+
+export type FnType = () => void;
