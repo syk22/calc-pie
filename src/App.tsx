43afdd9ea9870959531fd4b81ff0b5fce2de79ee
SayakaNakajima <andsayaka@gmail.com>
2023-11-09 10:43:27 +0900

    ルーター追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { MultipleView } from './pages/MultipleView';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { MultipleProvider } from './providers/MultipleProvider';
+import { TopPageView } from './pages/TopPageView';
+import { MultipleView } from './pages/MultipleView';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <TopPageView />,
+    },
+    {
+      path: '/multiple',
+      element: <MultipleView />,
+    },
+  ]);
+
   return (
     <MultipleProvider>
-      <MultipleView />
+      <RouterProvider router={router} />
     </MultipleProvider>
   );
 };
